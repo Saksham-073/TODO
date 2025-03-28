e53e233a68fdb78023752e6b0771dd2b460c2060
Saksham jamwal <130532990+Saksham-073@users.noreply.github.com>
2025-03-28 13:36:39 +0530

    Delete src directory

--- src/api/weatherService.js
+++
@@ -1,7 +0,0 @@
-export const fetchWeather = async (location) => {
-    const response = await fetch(`http://localhost:5000/api/weather/${location}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch weather data');
-    }
-    return await response.json();
-  };
